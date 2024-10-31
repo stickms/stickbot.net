bda7607a104cc1f095d2907fbec0f0147a168107
stickfr <20114698+stickfr@users.noreply.github.com>
2024-10-31 12:01:41 -0400

    WIP steam profile lookup page

--- client/src/components/steam-profile.tsx
+++ client/src/components/steam-profile.tsx
@@ -1,3 +1,16 @@
-function SteamProfile() {}
+import { Card } from "@radix-ui/themes";
+import { useEffect } from "react";
+
+function SteamProfile({ steamid }: { steamid: string }) {
+  useEffect(() => {
+    console.log(steamid);
+  }, [ steamid ]);
+
+  return (
+    <Card className='mx-2 mb-2 min-h-50 min-w-60 max-w-[80vw] flex-shrink-0'>
+      {steamid}
+    </Card>
+  );
+}
 
 export default SteamProfile;
--- client/src/routes/profile-lookup.tsx
+++ client/src/routes/profile-lookup.tsx
@@ -1,16 +1,43 @@
-import { Flex, Text, TextField } from '@radix-ui/themes';
+import { Container, Flex, Grid, Text, TextField } from '@radix-ui/themes';
+import { useState } from 'react';
+import SteamProfile from '../components/steam-profile';
 
 function ProfileLookup() {
+  const [ query, setQuery ] = useState<string>('');
+  const [ profiles, setProfiles ] = useState<string[]>([])
+  
+  const handleKeyPress = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === 'Enter') {
+      e.stopPropagation();
+      setProfiles((prev) => [ query, ...prev ]);
+      setQuery('');
+    }
+  }
+
   return (
-    <Flex className='items-center justify-center h-screen flex-col gap-y-24'>
-      <Text className='text-3xl'>Steam Profile Lookup</Text>
-      <Flex className='items-center justify-center flex-col gap-y-4'>
+    <Grid className='grid-rows-2 grid-cols-1 w-screen h-screen'>
+      <Flex className='items-center justify-center flex-col gap-y-24 mt-[20vh]'>
+        <Text className='text-3xl'>Steam Profile Lookup</Text>
         <TextField.Root
-          className='w-96'
-          placeholder='Lookup a Steam Profile...'
+            className='w-96 max-w-[80vw]'
+            placeholder='Lookup a Steam Profile...'
+            value={query}
+            onChange={ (e) => setQuery(e.target.value) }
+            onKeyDown={handleKeyPress}
         />
       </Flex>
-    </Flex>
+      <Flex className='items-center justify-center w-full h-full'>
+        <Container className='w-96 max-w-[80vw]'>
+          <Flex className='items-center justify-center flex-col gap-y-5 w-full max-h-[30vh] overflow-y-auto pt-2'>
+            { 
+              profiles.map((p) => (
+                <SteamProfile steamid={p} />
+              )) 
+            }
+          </Flex>
+        </Container>
+      </Flex>
+    </Grid>
   );
 }
 
