767b7517a4ada9814d5e3c7868150eb05d96c936
stickfr <20114698+stickfr@users.noreply.github.com>
2024-10-31 16:54:08 -0400

    Fixed errors in steam profile cards

--- client/src/components/steam-profile.tsx
+++ client/src/components/steam-profile.tsx
@@ -7,24 +7,6 @@ type SteamProfileProps = {
   setDisabled: (disabled: boolean) => void;
 };
 
-type ProfileSectionProps = {
-  title: string,
-  body: string
-};
-
-function ProfileSection({ title, body }: ProfileSectionProps) {
-  return (
-    <Box className='min-w-36 flex-grow'>
-      <Box className='w-full'>
-        <Text size='2' weight='bold'>{title}</Text>
-      </Box>
-      <Box className='w-full whitespace-pre-line'>
-        <Text size='2'>{body}</Text>
-      </Box>
-    </Box>
-  );
-}
-
 function SteamIdList({ summary }: {summary: SteamProfileSummary}) {
   return (
     <Box className='min-w-36 flex-grow'>
@@ -46,7 +28,41 @@ function AlertList({ summary }: {summary: SteamProfileSummary}) {
   return (
     <Box className='min-w-36 flex-grow'>
       <Box className='w-full'>
-        <Text size='2' weight='bold'>Steam IDs</Text>
+        <Text size='2' weight='bold'>Alerts</Text>
+      </Box>
+      <Box className='w-full whitespace-pre-line'>
+        <Text size='2'>
+          {
+            summary.steamid
+          }
+        </Text>
+      </Box>
+    </Box>
+  );
+}
+
+function QuickLinks({ summary }: {summary: SteamProfileSummary}) {
+  return (
+    <Box className='min-w-36 flex-grow'>
+      <Box className='w-full'>
+        <Text size='2' weight='bold'>Quick Links</Text>
+      </Box>
+      <Box className='w-full whitespace-pre-line'>
+        <Text size='2'>
+          {
+            summary.steamid
+          }
+        </Text>
+      </Box>
+    </Box>
+  );
+}
+
+function Sourcebans({ summary }: {summary: SteamProfileSummary}) {
+  return (
+    <Box className='min-w-36 flex-grow'>
+      <Box className='w-full'>
+        <Text size='2' weight='bold'>Sourcebans</Text>
       </Box>
       <Box className='w-full whitespace-pre-line'>
         <Text size='2'>
@@ -61,11 +77,7 @@ function AlertList({ summary }: {summary: SteamProfileSummary}) {
 
 function SteamProfile({ steamid, setDisabled }: SteamProfileProps) {
   const [ summary, setSummary ] = useState<SteamProfileSummary>();
-
   const [ isError, setIsError ] = useState<boolean>(false);
-
-  const [ quickLinks, setQuickLinks ] = useState<string>('');
-  const [ sourcebans, setSourcebans ] = useState<string>('');
 
   useEffect(() => {
     fetch(`http://localhost:3000/api/lookup/${steamid}`, { signal: AbortSignal.timeout(500) })
@@ -93,7 +105,7 @@ function SteamProfile({ steamid, setDisabled }: SteamProfileProps) {
   if (isError) {
     return (
       <Card className='mx-4 mb-4 max-w-[80vw] min-h-[300px] w-[600px]'>
-        <Text>
+        <Text color='ruby' weight='bold'>
           Error
         </Text>
       </Card>
@@ -118,8 +130,8 @@ function SteamProfile({ steamid, setDisabled }: SteamProfileProps) {
             <Flex className='gap-5 flex-wrap'>
               <SteamIdList summary={summary} />
               <AlertList summary={summary} />
-              <ProfileSection title='Quick Links' body={quickLinks} />
-              <ProfileSection title='Sourcebans' body={sourcebans} />
+              <QuickLinks summary={summary} />
+              <Sourcebans summary={summary} />
             </Flex>
           </Box>
         </Flex>
