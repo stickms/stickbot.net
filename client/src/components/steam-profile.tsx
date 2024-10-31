da6e996d5bdb33eff2eb6662a0487666d883e6dd
stickms <20114698+stickms@users.noreply.github.com>
2024-10-31 13:16:32 -0400

    Updated look of steam profile cards TODO: add API integration

--- client/src/components/steam-profile.tsx
+++ client/src/components/steam-profile.tsx
@@ -1,14 +1,46 @@
-import { Card } from "@radix-ui/themes";
-import { useEffect } from "react";
+import { Avatar, Box, Card, Flex, Text } from '@radix-ui/themes';
+import { useEffect } from 'react';
 
-function SteamProfile({ steamid }: { steamid: string }) {
+type SteamProfileProps = {
+  steamid: string;
+  setDisabled: (disabled: boolean) => void;
+};
+
+function SteamProfile({ steamid, setDisabled }: SteamProfileProps) {
   useEffect(() => {
+    setDisabled(false);
     console.log(steamid);
-  }, [ steamid ]);
+  }, [steamid, setDisabled]);
 
   return (
-    <Card className='mx-2 mb-2 min-h-50 min-w-60 max-w-[80vw] flex-shrink-0'>
-      {steamid}
+    <Card className='mx-4 mb-4 max-w-[80vw] min-h-[300px] w-[800px]'>
+      <Flex className='gap-3 items-start'>
+        <Avatar src='https://imgur.com/uO7rwHu.png' fallback='T' />
+        <Box>
+          <Box className='w-full'>
+            <Text size='3' weight='bold'>
+              Profile Name - {steamid}
+            </Text>
+          </Box>
+          <Flex className='gap-3 flex-wrap'>
+            <Box className='w-48 flex-grow'>
+              <Text size='2'>1</Text>
+            </Box>
+            <Box className='w-48 flex-grow'>
+              <Text size='2'>2</Text>
+            </Box>
+            <Box className='w-48 flex-grow'>
+              <Text size='2'>3</Text>
+            </Box>
+            <Box className='w-48 flex-grow'>
+              <Text size='2'>4</Text>
+            </Box>
+            <Box className='w-48 flex-grow'>
+              <Text size='2'>5</Text>
+            </Box>
+          </Flex>
+        </Box>
+      </Flex>
     </Card>
   );
 }
--- client/src/routes/profile-lookup.tsx
+++ client/src/routes/profile-lookup.tsx
@@ -1,41 +1,49 @@
-import { Container, Flex, Grid, Text, TextField } from '@radix-ui/themes';
+import { Flex, Grid, ScrollArea, Text, TextField } from '@radix-ui/themes';
 import { useState } from 'react';
 import SteamProfile from '../components/steam-profile';
 
 function ProfileLookup() {
-  const [ query, setQuery ] = useState<string>('');
-  const [ profiles, setProfiles ] = useState<string[]>([])
-  
+  const [query, setQuery] = useState<string>('');
+  const [disabled, setDisabled] = useState<boolean>(false);
+  const [profiles, setProfiles] = useState<string[]>([]);
+
   const handleKeyPress = (e: React.KeyboardEvent<HTMLInputElement>) => {
     if (e.key === 'Enter') {
       e.stopPropagation();
-      setProfiles((prev) => [ query, ...prev ]);
+      setProfiles((prev) => [query, ...prev]);
       setQuery('');
+      setDisabled(true);
     }
-  }
+  };
 
   return (
     <Grid className='grid-rows-2 grid-cols-1 w-screen h-screen'>
       <Flex className='items-center justify-center flex-col gap-y-24 mt-[20vh]'>
-        <Text className='text-3xl'>Steam Profile Lookup</Text>
+        <Text className='text-3xl text-center'>Steam Profile Lookup</Text>
         <TextField.Root
-            className='w-96 max-w-[80vw]'
-            placeholder='Lookup a Steam Profile...'
-            value={query}
-            onChange={ (e) => setQuery(e.target.value) }
-            onKeyDown={handleKeyPress}
+          className='w-96 max-w-[80vw]'
+          placeholder='Lookup a Steam Profile...'
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+          onKeyDown={handleKeyPress}
+          disabled={disabled}
         />
       </Flex>
-      <Flex className='items-center justify-center w-full h-full'>
-        <Container className='w-96 max-w-[80vw]'>
-          <Flex className='items-center justify-center flex-col gap-y-5 w-full max-h-[30vh] overflow-y-auto pt-2'>
-            { 
-              profiles.map((p) => (
-                <SteamProfile steamid={p} />
-              )) 
-            }
+      <Flex className='items-center justify-center'>
+        <ScrollArea
+          scrollbars='vertical'
+          className='w-auto max-w-[80vw] max-h-[40vh]'
+        >
+          <Flex className='items-center justify-center flex-col'>
+            {profiles.map((p, i) => (
+              <SteamProfile
+                key={profiles.length - i}
+                steamid={p}
+                setDisabled={setDisabled}
+              />
+            ))}
           </Flex>
-        </Container>
+        </ScrollArea>
       </Flex>
     </Grid>
   );
