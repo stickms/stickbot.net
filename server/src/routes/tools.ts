e938e47feb61c3838392523f0816b6480ca0c3ea
Stick <20114698+stickms@users.noreply.github.com>
2025-01-12 21:13:39 -0500

    Added more clients

--- server/src/routes/tools.ts
+++ server/src/routes/tools.ts
@@ -56,7 +56,7 @@ tools_route.get('/tools/youtube-dl', async (c) => {
   const video_url = `http://www.youtube.com/watch?v=${video_id}`;
 
   const video_info = await ytdl.getInfo(video_url, {
-    playerClients: ['IOS'],
+    playerClients: ['IOS', 'WEB'],
     agent 
   });
 
@@ -84,7 +84,7 @@ tools_route.get('/tools/youtube-dl', async (c) => {
 
   const video_stream = ytdl(video_url, {
     format: video_format,
-    playerClients: ['IOS'],
+    playerClients: ['IOS', 'WEB'],
     agent
   }).on('error', () => {
     throw new HTTPException(403, {
@@ -94,7 +94,7 @@ tools_route.get('/tools/youtube-dl', async (c) => {
 
   const audio_stream = ytdl(video_url, {
     format: audio_format,
-    playerClients: ['IOS'],
+    playerClients: ['IOS', 'WEB'],
     agent
   }).on('error', () => {
     throw new HTTPException(403, {
