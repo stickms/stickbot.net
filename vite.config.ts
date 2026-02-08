c653527e0e7c2fc12f083520fe001cb6d285f7ad
Stick <20114698+stickms@users.noreply.github.com>
2026-02-08 13:16:24 -0500

    Added nitro for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import tsConfigPaths from 'vite-tsconfig-paths'
 import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 import viteReact from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { nitro } from 'nitro/vite'
 
 export default defineConfig({
   server: {
@@ -12,15 +13,16 @@ export default defineConfig({
     tailwindcss(),
     tsConfigPaths(),
     tanstackStart(),
+    nitro(),
     // react's vite plugin must come after start's vite plugin
-    viteReact(),
+    viteReact()
   ],
   optimizeDeps: {
     // Exclude this server-side dependency to avoid bundling errors
-    exclude: ['mongodb-client-encryption'],
+    exclude: ['mongodb-client-encryption']
   },
   ssr: {
     // Ensure this module is externalized during server-side rendering
-    external: ['mongodb-client-encryption'],
+    external: ['mongodb-client-encryption']
   },
 });
